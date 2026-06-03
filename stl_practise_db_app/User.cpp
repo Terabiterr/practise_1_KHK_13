ebb3bdc309c60ee922222f65d6aa70c4d993a421
Mario <mario@gmail.com>
2026-06-03 11:26:55 +0300

    fdsfdsf

--- stl_practise_db_app/User.cpp
+++ stl_practise_db_app/User.cpp
@@ -1,9 +1,17 @@
 #pragma once
 #include <string>
+#include <sstream>
 using namespace std;
 struct User {
-	
 	int id;
 	string name;
+
+	string to_string() {
+		stringstream ss;
+		ss
+			<< "id: " << id
+			<< ", name: " << name
+			<< endl;
+	}
 
 };
